f8b0879192770238069249e760c5869581630544
elisacch <elisa.sacchelli@icloud.com>
2023-09-05 19:15:02 +0100

    Create events model and connect to events page

--- ecoevents/urls.py
+++ ecoevents/urls.py
@@ -4,4 +4,5 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('events/', views.events, name='events'),
 ]
--- ecoevents/views.py
+++ ecoevents/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
+from .models import Ecoevent
 
 
 def home(request):
     return render(request, 'index.html', {})
+
+
+def events(request):
+    all_events = Ecoevent.objects.all
+    context = {'all_events': all_events}
+    return render(request, 'events.html', context)

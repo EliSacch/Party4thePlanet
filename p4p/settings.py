d6618a1e35516c9e61f3d76f6a62d5440fe22e69
elisacch <elisa.sacchelli@icloud.com>
2023-09-06 13:27:24 +0100

    Migrate database

--- p4p/settings.py
+++ p4p/settings.py
@@ -12,10 +12,12 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 import os
+import dj_database_url
+
 if os.path.isfile('env.py'):
      import env
 
-development = os.environ.get('DEVELOPMENT', False)
+development = os.environ.get('DEV', False)
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -81,12 +83,17 @@ WSGI_APPLICATION = 'p4p.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+if 'DEV' in os.environ:
+     DATABASES = {
+         'default': {
+             'ENGINE': 'django.db.backends.sqlite3',
+             'NAME': BASE_DIR / 'db.sqlite3',
+         }
+     }
+else:
+     DATABASES = {
+         'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
+     }
 
 
 # Password validation

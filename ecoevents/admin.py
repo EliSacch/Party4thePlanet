83b7c46b16a0dffcafde18fa792cc39f7bc1ae01
kurt mcgoona <kurtm36@gmail.com>
2023-09-06 15:02:52 +0100

    Prepairing to merge to main branch

--- ecoevents/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Ecoevent
-
-
-@admin.register(Ecoevent)
-class EcoeventAdmin(admin.ModelAdmin):
-    list_filter = ('location', 'category', 'start_datetime')
-    readonly_fields = ('created_on', 'modified_on', 'pk')

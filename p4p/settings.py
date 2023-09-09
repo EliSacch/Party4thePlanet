9e053df47db0ab66d9924e9e5f682eafce841dbf
Indrek Mannik <intc21@gmail.com>
2023-09-09 12:15:48 +0100

    Set up and style email for password reset

--- p4p/settings.py
+++ p4p/settings.py
@@ -135,6 +135,13 @@ LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/events/'
 ACCOUNT_SIGNUP_REDIRECT_URL = "/events/"
 
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'intc21@gmail.com'
+EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD")
+
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
